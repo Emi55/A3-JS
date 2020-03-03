5419ae0ead034976ffcfcbca85706db04054a336
Emi55 <57016328+Emi55@users.noreply.github.com>
2020-03-03 13:25:42 -0500

    Add files via upload

---
+++ Index.js
@@ -0,0 +1,125 @@
+const menus = [
+    {
+        title: 'Menu 1',
+        soup: {
+            name: 'Clam Chowder',
+            price: '3.25'
+        
+        },
+        salad: {
+            name: 'Caesar',
+            price: '1.25'
+        
+        },
+        lighterFare: {
+            name: 'Vegetable Biryani',
+            price: '4.95'
+        
+        },
+        entree: {
+            name: 'Breaded Chicken on Bun',
+            price: '5.95'
+        
+        }
+    },
+
+{
+    title: 'Menu 2',
+        soup: {
+            name: 'Chicken Soup',
+            price: '3.25'
+        
+        },
+        salad: {
+            name: 'Greek',
+            price: '1.25'
+        
+        },
+        lighterFare: {
+            name: 'Burger Fries',
+            price: '4.95'
+        
+        },
+        entree: {
+            name: 'Fish and Chips',
+            price: '5.95'
+    
+        }
+    },
+
+{
+    title: 'Menu 3',
+        soup: {
+            name: 'Squash and Pumkin',
+            price: '3.25'
+        
+        },
+        salad: {
+            name: 'Medditeranean',
+            price: '1.25'
+        
+        },
+        lighterFare: {
+            name: 'Ham and Cheese Panini',
+            price: '4.95'
+        
+        },
+        entree: {
+            name: 'Butter Chicken Pilaf',
+            price: '5.95'
+    
+        }
+    },
+
+{
+  
+    title: 'Menu 4',
+        soup: {
+            name: 'Tomato',
+            price: '3.25'
+        
+        },
+        salad: {
+            name: 'Couscous Feta',
+            price: '1.25'
+        
+        },
+        lighterFare: {
+            name: 'Pulled Pork on bun',
+            price: '4.95'
+        
+        },
+        entree: {
+            name: 'Beef Brocoli fried rice',
+            price: '5.95'
+    
+        }
+    },
+{
+    title: 'Menu 5',
+    soup: {
+        name: 'Corn',
+        price: '3.25'
+    
+    },
+    salad: {
+        name: 'Julienne',
+        price: '1.25'
+    
+    },
+    lighterFare: {
+        name: 'Madrass Wrap',
+        price: '4.95'
+    
+    },
+    entree: {
+        name: 'Samousa',
+        price: '5.95'
+
+    }
+},
+
+
+
+]
+
